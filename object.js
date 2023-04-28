60aca0e9e540245b49391c9b0de4a88ca7877a47
Gloria-Lado <glorialadoo@gmail.com>
2023-04-28 19:18:11 +0300

    files

--- object.js
+++ object.js
@@ -71,3 +71,25 @@ let bestStudents = (students) => {
     return bestStudent;
   };
   console.log(bestStudents(students))
+
+//   Given an object representing a car, with properties for the make, model, 
+// year, and a method to display the car's information, write a function that takes 
+// the car object and adds a new method to the object called age. The age method should 
+// return the current age of the car based on the current year and the car's year property.
+const car = {
+    make: 'Ford',
+    model: 'Ranger',
+    year: 2023,
+    displayInfo: function() {
+      console.log(`Make: ${this.make}, Model: ${this.model}, Year: ${this.year}`);
+    },
+  };
+
+  let newMethodage = (car) => {
+    let currentYear = new Date().getFullYear();
+    car.age = function() {
+      return currentYear - this.year;
+    };
+  };
+  newMethodage(car);
+  console.log(car.age());
